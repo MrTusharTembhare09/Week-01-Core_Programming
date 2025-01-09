d9d097e04b7ed30e2c27b465e7e401354a95881c
MrTusharTembhare09 <tushartembhare147@gmail.com>
2025-01-09 22:34:44 +0530

    Added DistanceConversion.java

---
+++ Level 1 Practice Programs/DistanceConversion.java
@@ -0,0 +1,19 @@
+// Program 3: Convert Distance from Kilometers to Miles
+public class DistanceConversion {
+    public static void main(String[] args) {
+        // Define distance in kilometers
+        double kilometers = 10.8;
+
+        // Conversion factor
+        double kmToMiles = 1.6;
+
+        // Convert to miles
+        double miles = kilometers / kmToMiles;
+
+        // Print the result
+        System.out.println("The distance " + kilometers + " km in miles is " + miles);
+    }
+}
+		
+		
+		
