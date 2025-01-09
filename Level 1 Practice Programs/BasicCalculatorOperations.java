15e96e42cf42e278a12df97caf3f741105d74e37
MrTusharTembhare09 <tushartembhare147@gmail.com>
2025-01-09 22:34:43 +0530

    Added BasicCalculatorOperations.java

---
+++ Level 1 Practice Programs/BasicCalculatorOperations.java
@@ -0,0 +1,20 @@
+// Program 11: Basic Calculator
+import java.util.*;
+
+public class BasicCalculatorOperations {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // Take user input for numbers
+        System.out.print("Enter first number: ");
+        double number1 = input.nextDouble();
+        System.out.print("Enter second number: ");
+        double number2 = input.nextDouble();
+        // Perform operations
+        double addition = number1 + number2;
+        double subtraction = number1 - number2;
+        double multiplication = number1 * number2;
+        double division = number1 / number2;
+        // Print the result
+        System.out.println("The addition, subtraction, multiplication and division value of 2 numbers " + number1 + " and " + number2 + " is " + addition + ", " + subtraction + ", " + multiplication + ", and " + division);
+    }
+}
