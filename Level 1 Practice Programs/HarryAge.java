02e3e771dcd1f6a95651730b1812c112a9f3aa5c
MrTusharTembhare09 <tushartembhare147@gmail.com>
2025-01-09 22:34:45 +0530

    Added HarryAge.java

---
+++ Level 1 Practice Programs/HarryAge.java
@@ -0,0 +1,14 @@
+// Program 1: Calculate Harry's Age
+public class HarrysAge {
+    public static void main(String[] args) {
+        // Define the birth year and current year
+        int birthYear = 2000;
+        int currentYear = 2024;
+
+        // Calculate age
+        int age = currentYear - birthYear;
+
+        // Print the result
+        System.out.println("Harry's age in 2024 is " + age);
+    }
+}
