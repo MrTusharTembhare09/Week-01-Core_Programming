be11173585bfd01da63c2cd70490f59db488a68a
MrTusharTembhare09 <tushartembhare147@gmail.com>
2025-01-09 22:34:45 +0530

    Added SamsAverage.java

---
+++ Level 1 Practice Programs/SamsAverage.java
@@ -0,0 +1,15 @@
+// Program 2: Calculate Sam's Average Mark
+public class SamsAverage {
+    public static void main(String[] args) {
+        // Define marks
+        int maths = 94;
+        int physics = 95;
+        int chemistry = 96;
+
+        // Calculate average
+        double average = (maths + physics + chemistry) / 3.0;
+
+        // Print the result
+        System.out.println("Sam’s average mark in PCM is " + average);
+    }
+}
