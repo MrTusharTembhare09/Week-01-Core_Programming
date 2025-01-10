debb438fb45b2c9541707e2ac14fd7844169122d
MrTusharTembhare09 <tushartembhare147@gmail.com>
2025-01-10 10:42:40 +0530

    Added DivisibleByFive01.java

---
+++ Level 1 Practice Programs/DivisibleByFive01.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class DivisibleByFive01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        // Taking input from the user
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        
+        // Checking divisibility by 5
+        if (number % 5 == 0) {
+            System.out.println("Is the number " + number + " divisible by 5? Yes");
+        } else {
+            System.out.println("Is the number " + number + " divisible by 5? No");
+        }
+    }
+}
+
